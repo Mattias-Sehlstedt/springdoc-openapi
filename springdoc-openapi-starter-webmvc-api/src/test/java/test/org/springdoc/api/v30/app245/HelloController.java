79fe3c5c7c93b1fc26c028f679113f1f4ce235cf
Mattias-Sehlstedt <60173714+Mattias-Sehlstedt@users.noreply.github.com>
2025-12-05 18:32:41 +0100

    Add example

---
+++ springdoc-openapi-starter-webmvc-api/src/test/java/test/org/springdoc/api/v30/app245/HelloController.java
@@ -0,0 +1,87 @@
+/*
+ *
+ *  *
+ *  *  *
+ *  *  *  *
+ *  *  *  *  *
+ *  *  *  *  *  * Copyright 2019-2025 the original author or authors.
+ *  *  *  *  *  *
+ *  *  *  *  *  * Licensed under the Apache License, Version 2.0 (the "License");
+ *  *  *  *  *  * you may not use this file except in compliance with the License.
+ *  *  *  *  *  * You may obtain a copy of the License at
+ *  *  *  *  *  *
+ *  *  *  *  *  *      https://www.apache.org/licenses/LICENSE-2.0
+ *  *  *  *  *  *
+ *  *  *  *  *  * Unless required by applicable law or agreed to in writing, software
+ *  *  *  *  *  * distributed under the License is distributed on an "AS IS" BASIS,
+ *  *  *  *  *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  *  *  *  *  * See the License for the specific language governing permissions and
+ *  *  *  *  *  * limitations under the License.
+ *  *  *  *  *
+ *  *  *  *
+ *  *  *
+ *  *
+ *
+ */
+
+package test.org.springdoc.api.v30.app245;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    public record SampleRequest(
+            @Schema(type = "integer")
+            String hi,
+            @Schema(type = "number")
+            String bye,
+            @Schema(type = "boolean")
+            Boolean field) {
+    }
+
+    @PostMapping(value = "v1/endpoint")
+    public SampleResponse endpoint(@RequestBody SampleRequest request) {
+        return null;
+    }
+
+    public static class SampleResponse {
+
+        @Schema(description = "field", type = "boolean", example = "true")
+        private Boolean field;
+
+        @Schema(description = "count", type = "integer", example = "22")
+        private Integer count;
+
+        private String note;
+
+        public Boolean getField() {
+            return field;
+        }
+
+        public void setField(Boolean field) {
+            this.field = field;
+        }
+
+        public Integer getCount() {
+            return count;
+        }
+
+        public void setCount(Integer count) {
+            this.count = count;
+        }
+
+        public String getNote() {
+            return note;
+        }
+
+        public void setNote(String note) {
+            this.note = note;
+        }
+    }
+
+}
+
