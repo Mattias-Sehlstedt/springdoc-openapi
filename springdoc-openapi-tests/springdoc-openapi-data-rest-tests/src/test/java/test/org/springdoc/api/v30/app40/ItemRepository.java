ea08347f92678800ff713058d7f5620f8e3aced5
Mattias-Sehlstedt <60173714+Mattias-Sehlstedt@users.noreply.github.com>
2026-08-14 18:08:37 +0200

    Reproduce issues

---
+++ springdoc-openapi-tests/springdoc-openapi-data-rest-tests/src/test/java/test/org/springdoc/api/v30/app40/ItemRepository.java
@@ -0,0 +1,7 @@
+package test.org.springdoc.api.v30.app40;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ItemRepository extends JpaRepository<Item, Long> {
+
+}
---
+++ springdoc-openapi-tests/springdoc-openapi-data-rest-tests/src/test/java/test/org/springdoc/api/v31/app40/ItemRepository.java
@@ -0,0 +1,7 @@
+package test.org.springdoc.api.v31.app40;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ItemRepository extends JpaRepository<Item, Long> {
+
+}
